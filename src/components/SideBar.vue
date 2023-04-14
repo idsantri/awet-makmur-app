1022d5cc0e7a682b64fc26c2b6c3d4c9abc5e0db
Usman <oets.man@gmail.com>
2023-04-15 01:20:17 +0700

    sidebar add store

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,16 +1,11 @@
 <template>
   <q-list class="text-teal-1">
+
     <q-item-label header class="text-teal-10">
-      <div class="text-body1">Kategori</div>
+      <div class="text-body1">Kategori Produk</div>
     </q-item-label>
 
-    <q-item
-      clickable
-      v-ripple
-      v-for="(category, index) in categories"
-      :key="index"
-      :to="category.link"
-    >
+    <q-item clickable v-ripple v-for="(category, index) in categories" :key="index" :to="category.link">
       <q-item-section avatar>
         <q-icon color="teal-1" :name="category.icon" />
       </q-item-section>
@@ -22,9 +17,24 @@
 
     <q-separator dark />
     <q-item-label header class="text-teal-10">
-      <div class="text-body1">Fitur</div>
+      <div class="text-body1">Transaksi</div>
     </q-item-label>
 
+    <q-item clickable v-ripple v-for="(store, index) in stores" :key="index" :to="store.link">
+      <q-item-section avatar>
+        <q-icon color="teal-1" :name="store.icon" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ store.name }}</q-item-label>
+        <q-item-label caption>{{ store.caption }}</q-item-label>
+      </q-item-section>
+    </q-item>
+
+
+    <q-separator dark />
+    <q-item-label header class="text-teal-10">
+      <div class="text-body1">Fitur</div>
+    </q-item-label>
     <q-item clickable v-ripple>
       <q-item-section avatar>
         <q-icon color="teal-1" name="card_giftcard" />
@@ -34,6 +44,7 @@
         <q-item-label caption></q-item-label>
       </q-item-section>
     </q-item>
+
   </q-list>
 </template>
 
@@ -56,6 +67,21 @@ const categories = [
     caption: "",
     icon: "inventory_2",
     link: "/products/categories/lemari",
+  },
+];
+
+const stores = [
+  {
+    name: "Toko 1",
+    caption: "",
+    icon: "store",
+    link: "/orders/stores/1",
+  },
+  {
+    name: "Toko 2",
+    caption: "",
+    icon: "store",
+    link: "/orders/stores/2",
   },
 ];
 </script>
