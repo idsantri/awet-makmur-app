ae697aabed9e252cc136568e4ec4d42bdb564333
Usman <oets.man@gmail.com>
2023-05-11 23:11:16 +0700

    delete toml + change theme

--- quasar.config.js
+++ quasar.config.js
@@ -164,7 +164,7 @@ module.exports = configure(function (ctx) {
         display: "standalone",
         orientation: "portrait",
         background_color: "#ffffff",
-        theme_color: "#027be3",
+        theme_color: "#26a69a",
         icons: [
           {
             src: "icons/icon-128x128.png",
