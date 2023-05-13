fc3f160e97fcadea3a084dd79c6680d2051a482a
Usman <oets.man@gmail.com>
2023-05-13 11:18:29 +0700

    redirect / to /home

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ const routes = [
     children: [
       {
         path: "",
-        component: () => import("pages/IndexPage.vue")
+        redirect: "/home"
       },
       {
         path: "home",
