e6bccfd2e2afc66a1da26253482c7b2f06cebfe7
Usman <oets.man@gmail.com>
2023-06-08 11:53:44 +0700

    .

--- src/pages/home/HomeIndex.vue
+++ src/pages/home/HomeIndex.vue
@@ -4,7 +4,7 @@
     <div class="text-subtitle2 text-center">di Toko Awet Makmur</div>
   </div>
   <div class="text-center" style="margin-top: 20px;">
-    <q-avatar size="180px" square="">
+    <q-avatar size="150px" square="">
       <q-img src="../../assets/icon-c.png" />
     </q-avatar>
   </div>
