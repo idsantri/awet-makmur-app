8c9333deeab51cce8574da8250baf59351c50c96
oets <oets.man@gmail.com>
2026-07-24 18:42:34 +0700

    fix api production

--- quasar.config.js
+++ quasar.config.js
@@ -17,7 +17,7 @@ module.exports = configure(function (ctx) {
 	const baseURL_API =
 		env === "development"
 			? "http://localhost:8080"
-			: "https://api-am.baniasror.net";
+			: "https://am.idsantri.my.id";
 
 	return {
 		// https://v2.quasar.dev/quasar-cli-webpack/supporting-ts
